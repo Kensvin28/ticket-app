3f9208a7d7bd45fa98df67e44f53192fab70edc6
kensvin <kensvin28@gmail.com>
2025-10-07 20:07:40 +0700

    refactor: simplify PriorityDisplay component rendering logic

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -3,27 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PriorityDisplay = ({ priority }) => {
   return (
-    <div className="flex justify-start align-baseline">
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
-      />
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 1 ? "text-red-400" : "text-slate-400"}`}
-      />
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 2 ? "text-red-400" : "text-slate-400"}`}
-      />
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 3 ? "text-red-400" : "text-slate-400"}`}
-      />
-      <FontAwesomeIcon
-        icon={faFire}
-        className={`pr-1 ${priority > 4 ? "text-red-400" : "text-slate-400"}`}
-      />
+    <div className="flex items-center gap-1">
+      {[...Array(5)].map((_, index) => (
+        <FontAwesomeIcon
+          key={index}
+          className={`${index < priority ? "text-red-400" : "text-slate-300"}`}
+          icon={faFire}
+        />
+      ))}
     </div>
   );
 };
